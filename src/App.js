079ddd6c2660251d633b855922a62127a5f3eee0
Jaeeun98 <wodms4525@naver.com>
2021-07-11 00:03:35 +0900

    Title : Change
    진짜 수정. useEffect는 DOM 업데이트 후 함수를 실행하기 때문에 map을 사용하려면 초기값을 배열로 설정해야 함

--- src/App.js
+++ src/App.js
@@ -6,16 +6,13 @@ import axios from 'axios';
 
 function App() {
 
-  const [videos, setVideos] = useState(null);
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
 
     axios.get(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=5&key=${process.env.REACT_APP_YOUTUBE_API_KEY}`)
-      .then((response) => setVideos(response.data.items))
-    
-    console.log('jaeeun');
-    
-  }, [])
+      .then((response) => setVideos(response.data.items))  
+  }, videos)
 
   
   return (
